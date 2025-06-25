d0e43e2b860866f175cdafd568d7da1c2a5305c1
Yassine El motaouakkel <yel-mota@c2r4p2.1337.ma>
2025-06-25 15:46:05 +0100

    Seeyahh

--- minishell.c
+++ minishell.c
@@ -1,53 +1,28 @@
 #include <readline/readline.h>
 #include <stdlib.h>
-#include <readline/history.h>
+// #include <readline/history.h>
 
-size_t	ft_strlen(const char *str)
+void ft_parcing_it(char *str)
 {
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+  printf("%s\n", str);
 }
 
-char	*ft_strdup(const char *s)
+int ft_start()
 {
-	size_t		size;
-	char		*dest;
-  size_t      i;
+  char *str;
 
-	size = sizeof(char) * (ft_strlen(s) + 1);
-	dest = malloc(size);
-	if (dest == NULL)
-		return (NULL);
-  i = 0;
-  while (s[i])
+  while (1)
   {
-    dest[i] = s[i];
-    i++;
+    str = readline("mini-->>");
+    if (!str)
+      return (perror("minishell"), 1);
+    if (*str)
+      ft_parcing_it(str);
+    free(str);
   }
-  dest[i] = '\0';
-  return (dest);
 }
 
 int main ()
 {
-  char *str;
-
-  str = ft_strdup("");
-  while (1)
-  {
-    free(str);
-    str = readline("<-mini->");
-    if (!str)
-      return (1);
-    if (*str)
-      add_history(str);
-    printf("%s\n", str);
-  }
-  
-  rl_cleanup_after_signal();
-  rl_clear_history();
+  ft_start();
 }
