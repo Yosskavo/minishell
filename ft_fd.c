4737613f94d539c16a948dd2325f08878f949eb7
Yassine El motaouakkel <yel-mota@c1r8p4.1337.ma>
2025-08-07 14:45:32 +0100

    asdasdas

--- ft_fd.c
+++ ft_fd.c
@@ -1,35 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fork.c                                          :+:      :+:    :+:   */
+/*   ft_fd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yel-mota <yel-mota@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/06 21:26:41 by yel-mota          #+#    #+#             */
-/*   Updated: 2025/08/07 10:55:20 by yel-mota         ###   ########.fr       */
+/*   Created: 2025/08/07 10:51:11 by yel-mota          #+#    #+#             */
+/*   Updated: 2025/08/07 11:44:37 by yel-mota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-void	ft_before_forking(t_exec *execute)
+int	**ft_original_fd(void)
 {
-	int	child;
-	int	status;
+	static int	fd[2];
 
-	if (execute->next || execute->redi)
-		ft_dup();
-	while (execute)
-	{
-		if (execute->pipe)
-			ft_pipe();
-		if (execute->redi)
-			ft_redi();
-		child = ft_fork();
-		if (child < 0)
-			return ;
-		execute = execute->next;
-	}
-	waitpid(child, &status, 0);
-	wait(NULL);
+	return (&fd);
+}
+
+void	ft_restor_fd(void)
+{
+	int	*fd;
+
+	fd = *ft_original_fd();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+}
+
+void	ft_dup(void)
+{
+	int	**fd_orig;
+	int	fd[2];
+
+	fd[0] = dup(STDIN_FILENO);
+	fd[1] = dup(STDOUT_FILENO);
+	*fd_orig[0] = fd[0];
+	*fd_orig[1] = fd[0];
 }
